c60d048535244633a6046db32784b600ae67c44f
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-13 18:33:58 -0300

    create: debitCard payment

--- src/model/services/DebitService.java
+++ src/model/services/DebitService.java
@@ -1,4 +1,9 @@
 package model.services;
 
-public class DebitService {
+public class DebitService implements PaymentService{
+    private static final double DEBIT_FEE = 0.0125;
+    @Override
+    public double paymentProcessing(double value, int installments) {
+        return value + value * DEBIT_FEE;
+    }
 }
